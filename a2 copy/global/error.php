b8cf8c48107bc64f27ad5cd51c4f1326db23edd5
Leah Ramsier <ler13c@my.fsu.edu>
2015-11-02 16:32:38 -0500

    a4

--- a2 copy/global/error.php
+++ a2 copy/global/error.php
@@ -1,6 +1,3 @@
-<?php
-//database connection code goes here...
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +8,7 @@
 	<meta name="author" content="Mark K. Jowett, Ph.D.">
 	<link rel="icon" href="favicon.ico">
 
-	<title>CRSXXXX - AssignmentX</title>
+	<title>CRSXXXX Assignment/ProjectX</title>
 
 <!-- Include FontAwesome CSS to use feedback icons provided by FontAwesome -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -23,10 +20,6 @@
 <!-- Starter template for your own custom styling. -->
 <link href="css/starter-template.css" rel="stylesheet">
 
-<!-- jQuery DataTables: http://www.datatables.net/ //-->
-<link rel="stylesheet" type=""text/css" href="//cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
-<link rel="stylesheet" type=""text/css" href="//cdn.datatables.net/responsive/1.0.7/css/dataTables.responsive.css"/>
-
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -35,68 +28,31 @@
 
 </head>
 <body>
-
 	<?php include_once("../global/nav.php"); ?>
 	
-	<div class="container-fluid">
-		 <div class="starter-template">
+	<div class="container">
+			<div class="row">
+					<div class=".col-xs-12 col-sm-offset-2">
 						<div class="page-header">
 							<?php include_once("global/header.php"); ?>	
 						</div>
 
-						<h2>Pet Stores</h2>
+				<h2 class="top">Error!</h2>
 
-<a href="add_petstore.php">Add Pet Store</a>
-<br />
+<?php echo $error; ?>
+<br /><br />
 
- <div class="table-responsive">
-	 <table id="myTable" class="table table-striped table-condensed" >
-
-		 <!-- Code displaying PetStore data with Edit/Delete buttons goes here // -->
-
-	 </table>
- </div> <!-- end table-responsive -->
- 	
 <?php
-include_once "global/footer.php";
+require_once "global/footer.php";
 ?>
 
-		</div> <!-- starter-template -->
-  </div> <!-- end container -->
+					</div><!-- end grids -->
+			</div><!-- end row -->
+	</div><!-- end container -->
 
 	<!-- Bootstrap JavaScript
 	================================================== -->
 	<!-- Placed at end of document so pages load faster -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="//cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="//cdn.datatables.net/responsive/1.0.7/js/dataTables.responsive.min.js"></script>
-
-	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	<script src="js/ie10-viewport-bug-workaround.js"></script>
-
-			<script>
-	 $(document).ready(function(){
-		 $('#myTable').DataTable({
-	 //permit sorting (i.e., no sorting on last two columns: delete and edit)
-    "columns":
-		[
-      null,
-      null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-     { "orderable": false },
-     { "orderable": false }			
-    ]
-		 });
-});
-	</script>
-
 </body>
 </html>
--- a2 copy/test_connection.php
+++ a2 copy/test_connection.php
@@ -1,17 +1,14 @@
-<?php
-//database connection code goes here...
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="My online portfolio that illustrates skills acquired while working through various project requirements.">
+	<meta name="description" content="MySQL Database Server Information.">
 	<meta name="author" content="Mark K. Jowett, Ph.D.">
 	<link rel="icon" href="favicon.ico">
 
-	<title>CRSXXXX - AssignmentX</title>
+	<title>MySQL Database Server Information</title>
 
 <!-- Include FontAwesome CSS to use feedback icons provided by FontAwesome -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -37,33 +34,48 @@
 <body>
 
 	<?php include_once("../global/nav.php"); ?>
-	
-	<div class="container-fluid">
-		 <div class="starter-template">
+	<div class="container">
+			<div class="row">
+					<div class=".col-xs-12 col-sm-offset-2">
 						<div class="page-header">
-							<?php include_once("global/header.php"); ?>	
+							<h2>MySQL Database Server Information</h2>
 						</div>
+						
+<div class="table-responsive">
+<table id="myTable" class="table table-striped table-condensed" >
 
-						<h2>Pet Stores</h2>
+<?php
+require_once "./global/connection.php";
 
-<a href="add_petstore.php">Add Pet Store</a>
-<br />
+//find out which PDO drivers are available
+//print_r(PDO::getAvailableDrivers());
 
- <div class="table-responsive">
-	 <table id="myTable" class="table table-striped table-condensed" >
+$attributes = 
+array("AUTOCOMMIT", "CLIENT_VERSION", "CONNECTION_STATUS", "SERVER_INFO", "SERVER_VERSION");
+		
+foreach ($attributes as $val) 
+{
+		echo "<tr><th>$val:</th></tr>";
+?>
 
-		 <!-- Code displaying PetStore data with Edit/Delete buttons goes here // -->
+<?php
+		echo "<tr><td>" . $db->getAttribute(constant("PDO::ATTR_$val")) . "</td></tr>";
+?>
+
+  <?php
+	}
+    $db = null; 
+  ?>
 
 	 </table>
  </div> <!-- end table-responsive -->
- 	
+
 <?php
 include_once "global/footer.php";
 ?>
-
-		</div> <!-- starter-template -->
-  </div> <!-- end container -->
-
+					</div>
+			</div>
+	</div>
 	<!-- Bootstrap JavaScript
 	================================================== -->
 	<!-- Placed at end of document so pages load faster -->
@@ -75,25 +87,9 @@ include_once "global/footer.php";
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 	<script src="js/ie10-viewport-bug-workaround.js"></script>
 
-			<script>
+				<script>
 	 $(document).ready(function(){
 		 $('#myTable').DataTable({
-	 //permit sorting (i.e., no sorting on last two columns: delete and edit)
-    "columns":
-		[
-      null,
-      null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-     { "orderable": false },
-     { "orderable": false }			
-    ]
 		 });
 });
 	</script>
