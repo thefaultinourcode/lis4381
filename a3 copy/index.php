9ed190e7fe75dbf7d6965cb52b89309d78846eec
Leah Ramsier <ler13c@my.fsu.edu>
2015-11-16 18:53:28 -0500

    a5

--- a3 copy/index.php
+++ a3 copy/index.php
@@ -1,24 +1,28 @@
-<<!DOCTYPE html>
+<?php
+//database connection code goes here...
+?>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="My online portfolio that illustrates skills acquired while working through various project requirements.">
 	<meta name="author" content="Mark K. Jowett, Ph.D.">
-    <link rel="icon" href="favicon.ico">
+	<link rel="icon" href="favicon.ico">
 
-		<title>LIS4381 - Assignment3</title>		
+	<title>CRSXXXX - AssignmentX</title>
 
-<!-- Bootstrap core CSS -->
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- Include FontAwesome CSS to use feedback icons provided by FontAwesome -->
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
-<!-- Custom styles with this template -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 <link href="css/starter-template.css" rel="stylesheet">
+
+<!-- jQuery DataTables: http://www.datatables.net/ //-->
+<link rel="stylesheet" type=""text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.min.css"/>
+<link rel="stylesheet" type=""text/css" href="//cdn.datatables.net/responsive/1.0.4/css/dataTables.responsive.css"/>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
@@ -26,34 +30,54 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
-  </head>
+</head>
+<body>
 
-  <body>
-
-  <?php include_once("../global/nav.php"); ?>
+	<?php include_once("../global/nav.php"); ?>
 	
-		<div class="container">
-			<div class="starter-template">
-				<div class="page-header">
-					<?php include_once("global/header.php"); ?>	
-				</div>
+	<div class="container-fluid">
+		 <div class="starter-template">
+						<div class="page-header">
+							<?php include_once("global/header.php"); ?>	
+						</div>
 
-<img src="images/a3.png" class="img-responsive center-block"/><br/>
-				
-  <?php include_once "global/footer.php"; ?>
+						<h2>Pet Stores</h2>
+
+<a href="add_petstore.php">Add Pet Store</a>
+<br />
+
+<!-- Placeholder for responsive table if needed.  -->
+ <div class="table-responsive">
+	 <table id="myTable" class="table table-striped table-condensed" >
+
+		 <!-- Code displaying PetStore data with Edit/Delete buttons goes here // -->
+
+	 </table>
+ </div> <!-- end table-responsive -->
+ 	
+<?php
+include_once "global/footer.php";
+?>
 
 		</div> <!-- starter-template -->
-    </div> <!-- end container -->
+  </div> <!-- end container -->
 
-    <!-- Bootstrap core JavaScript: jQuery necessary for Bootstrap's JavaScript plugins
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+	<!-- Bootstrap JavaScript
+	================================================== -->
+	<!-- Placed at end of document so pages load faster -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="//cdn.datatables.net/1.10.5/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="//cdn.datatables.net/responsive/1.0.4/js/dataTables.responsive.min.js"></script>
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="js/ie10-viewport-bug-workaround.js"></script>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+	<script>
+	 $(document).ready(function(){
+    $('#myTable').DataTable();
+});
+	</script>
+
+</body>
 </html>
