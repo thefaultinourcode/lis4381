32ddc7e183712ed0c86b766132257665a0103723
Leah Ramsier <ler13c@my.fsu.edu>
2015-11-17 02:51:23 -0500

    a5

--- a2 copy/global/header.php
+++
@@ -1,4 +0,0 @@
-<h1>Assignment 5 </h1>
-<h3> Basic server-side validation </h3>
-<p class="lead">PHP server-side validation, along with prepared statements to help prevent SQL injection. <br/>
-Adds edit/delete functionality to A4 </p>
