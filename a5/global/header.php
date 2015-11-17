ba8fd34bb10be7e4395746356102c2b4090a0588
Leah Ramsier <ler13c@my.fsu.edu>
2015-11-17 03:28:36 -0500

    a5

--- a5/global/header.php
+++ a5/global/header.php
@@ -1,4 +1,4 @@
-<h1>Assignment 3</h1>
-    <h3>Database Design</h3>
-      <p class="lead">Database for the remaining LIS4381 assignments and projects.</p>
-
+<h1>Assignment 5 </h1>
+<h3> Basic server-side validation </h3>
+<p class="lead">PHP server-side validation, along with prepared statements to help prevent SQL injection. <br/>
+Adds edit/delete functionality to A4 </p>
