02da8b3d0deee6da498a9d5de884ffa5fabf41ce
Leah Ramsier <ler13c@my.fsu.edu>
2015-11-17 02:59:01 -0500

    a5

---
+++ a3 copy 2/index.php
@@ -0,0 +1,59 @@
+<<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="My online portfolio that illustrates skills acquired while working through various project requirements.">
+	<meta name="author" content="Mark K. Jowett, Ph.D.">
+    <link rel="icon" href="favicon.ico">
+
+		<title>LIS4381 - Assignment3</title>		
+
+<!-- Bootstrap core CSS -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Custom styles with this template -->
+<link href="css/starter-template.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+  </head>
+
+  <body>
+
+  <?php include_once("../global/nav.php"); ?>
+	
+		<div class="container">
+			<div class="starter-template">
+				<div class="page-header">
+					<?php include_once("global/header.php"); ?>	
+				</div>
+
+<img src="images/a3.png" class="img-responsive center-block"/><br/>
+				
+  <?php include_once "global/footer.php"; ?>
+
+		</div> <!-- starter-template -->
+    </div> <!-- end container -->
+
+    <!-- Bootstrap core JavaScript: jQuery necessary for Bootstrap's JavaScript plugins
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
