2ff1cdebac0e96a68d12e4b025694c3847910858
Leah Ramsier <ler13c@my.fsu.edu>
2015-11-17 03:02:44 -0500

    a5

---
+++ a3 copy/delete_petstore.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$pst_id_v = $_POST['pst_id'];
+
+require_once('global/connection.php');
+
+$query = 
+"DELETE FROM petstore
+WHERE pst_id = :pst_id_p";
+
+try 
+{
+	$statement = $db->prepare($query);
+	$statement->bindParam(':pst_id_p', $pst_id_v);
+	$row_count = $statement->execute();
+	$statement->closeCursor();
+
+}
+
+catch (PDOException $e)
+{
+	$error = $e->getMessage();
+	echo $error;
+}
+
+header('Location: index.php')
+?>
--- a3 copy/index.php
+++ a3 copy/index.php
@@ -1,24 +1,38 @@
-<<!DOCTYPE html>
+<?php
+//database connection code goes here...
+require_once "global/connection.php";
+
+$query = "SELECT * FROM petstore ORDER BY pst_id";
+
+$statement = $db->prepare($query);
+$statement->execute();
+
+?>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="My online portfolio that illustrates skills acquired while working through various project requirements.">
 	<meta name="author" content="Mark K. Jowett, Ph.D.">
-    <link rel="icon" href="favicon.ico">
+	<link rel="icon" href="favicon.ico">
 
-		<title>LIS4381 - Assignment3</title>		
+	<title>LIS4381 - Assignment4</title>
 
-<!-- Bootstrap core CSS -->
-<!-- Latest compiled and minified CSS -->
+<!-- Include FontAwesome CSS to use feedback icons provided by FontAwesome -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<!-- Bootstrap for responsive, mobile-first design. -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-<!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
-<!-- Custom styles with this template -->
+<!-- Starter template for your own custom styling. -->
 <link href="css/starter-template.css" rel="stylesheet">
+
+<!-- jQuery DataTables: http://www.datatables.net/ //-->
+<link rel="stylesheet" type=""text/css" href="//cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
+<link rel="stylesheet" type=""text/css" href="//cdn.datatables.net/responsive/1.0.7/css/dataTables.responsive.css"/>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
@@ -26,34 +40,131 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
-  </head>
+</head>
+<body>
 
-  <body>
-
-  <?php include_once("../global/nav.php"); ?>
+	<?php include_once("../global/nav.php"); ?>
 	
-		<div class="container">
-			<div class="starter-template">
-				<div class="page-header">
-					<?php include_once("global/header.php"); ?>	
-				</div>
+	<div class="container-fluid">
+		 <div class="starter-template">
+						<div class="page-header">
+							<?php include_once("global/header.php"); ?>	
+						</div>
 
-<img src="images/a3.png" class="img-responsive center-block"/><br/>
-				
-  <?php include_once "global/footer.php"; ?>
+						<h2>Pet Stores</h2>
+
+<a href="add_petstore.php">Add Pet Store</a>
+<br />
+
+ <div class="table-responsive">
+	 <table id="myTable" class="table table-striped table-condensed" >
+
+		 <!-- Code displaying PetStore data with Edit/Delete buttons goes here // -->
+	<thead>
+		<tr>
+		<th>Name</th>
+		<th>Street</th>
+		<th>City</th>
+		<th>State</th>
+		<th>Zip</th>
+		<th>Phone</th>
+		<th>Email</th>
+		<th>URL</th>
+		<th>YTD Sales</th>
+		<th>Notes</th>
+		<th>&nbsp;</th>
+		<th>&nbsp;</th>
+		</tr>
+	</thead>
+	<?php
+	$result = $statement->fetch();
+	while($result != null)
+	{
+?>
+
+	<td><?php echo htmlspecialchars($result['pst_name']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_street']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_city']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_state']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_zip']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_phone']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_email']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_url']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_ytd_sales']); ?></td>
+	<td><?php echo htmlspecialchars($result['pst_notes']); ?></td>
+
+	<td>
+		<form
+		onsubmit="return confirm('Do you really want to delete record?');"
+		action="delete_petstore.php"
+		method="post"
+		id="delete_petstore">
+
+		 <input type="hidden" name="pst_id" value="<?php echo $result['pst_id']; ?>" />
+		 <input type="submit" value="Delete"/>
+		</form>
+	</td>
+
+	<td>
+		<form action="edit_petstore.php" method="post" id="edit_petstore">
+
+		<input type="hidden" name="pst_id" value="<?php echo $result['pst_id']; ?>" />
+		<input type="submit" value="Edit" />
+	</form>
+	</td>
+
+</tr>
+
+<?php
+	$result = $statement->fetch();
+}
+$statement->closeCursor();
+$db = null;
+
+?>
+	 </table>
+ </div> <!-- end table-responsive -->
+ 	
+<?php
+include_once "global/footer.php";
+?>
 
 		</div> <!-- starter-template -->
-    </div> <!-- end container -->
+  </div> <!-- end container -->
 
-    <!-- Bootstrap core JavaScript: jQuery necessary for Bootstrap's JavaScript plugins
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+	<!-- Bootstrap JavaScript
+	================================================== -->
+	<!-- Placed at end of document so pages load faster -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="//cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="//cdn.datatables.net/responsive/1.0.7/js/dataTables.responsive.min.js"></script>
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="js/ie10-viewport-bug-workaround.js"></script>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+			<script>
+	 $(document).ready(function(){
+		 $('#myTable').DataTable({
+	 //permit sorting (i.e., no sorting on last two columns: delete and edit)
+    "columns":
+		[
+      null,
+      null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+     { "orderable": false },
+     { "orderable": false }			
+    ]
+		 });
+});
+	</script>
+
+</body>
 </html>
